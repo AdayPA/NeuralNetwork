3fffb4ac74fbc8fe6680732a9560a0430d956f0e
Adaypa <dyto_aday@hotmail.com>
2022-03-19 20:34:05 +0000

    epoch implemented

--- src/ann.cpp
+++ src/ann.cpp
@@ -7,14 +7,6 @@
 #include <thread>
 #include <process.h>
 
-void showVectorVals(std::string label, std::vector<double> &v) {
-    std::cout << label << " ";
-    for (unsigned i = 0; i < v.size(); ++i) {
-        std::cout << v[i] << " ";
-    }
-    std::cout << std::endl;
-}
-
 int main() {
     Dataset data("../data/input/test.txt", "../data/output/");
     // e.g., { 3, 2, 1 }
@@ -26,31 +18,39 @@ int main() {
     int trainingPass = 0;
     double total_data = (total_lines - 1) / 2;
     pBar bar;
+    int Epoch = 3;
     double increment =  100 / total_data;
-    while (!data.isEof()) {
-        ++trainingPass;
-        auto s = std::to_string(trainingPass);
-        data.writeOutput("Data " + s);
-        // Get new input data and feed it forward:
-        if (data.getNextInputs(inputVals) != topology[0]) {
-            break;
+    for (int i = 0; i < Epoch; i++) {
+        auto epo = std::to_string(i);
+        data.writeOutput("Epoch " + epo + "\n"); 
+        while (!data.isEof()) {
+            ++trainingPass;
+            auto s = std::to_string(trainingPass);
+            data.writeOutput("Data " + s);
+            // Get new input data and feed it forward:
+            if (data.getNextInputs(inputVals) != topology[0]) {
+                break;
+            }
+            data.writeEvolution(": Inputs:", inputVals);
+            myNet.feedForward(inputVals);
+            // Collect the net's actual output results:
+            myNet.getResults(resultVals);
+            data.writeEvolution("Outputs:", resultVals);
+            // Train the net what the outputs should have been:
+            data.getTargetOutputs(targetVals);
+            data.writeEvolution("Targets:", targetVals);
+            assert(targetVals.size() == topology.back());
+            myNet.backProp(targetVals);
+            // Report how well the training is working, average over recent samples:
+            data.writeOutputError(myNet.getRecentAverageError());
+            // bar.update(increment);
+            // bar.print();
         }
-        data.writeEvolution(": Inputs:", inputVals);
-        myNet.feedForward(inputVals);
-        // Collect the net's actual output results:
-        myNet.getResults(resultVals);
-        data.writeEvolution("Outputs:", resultVals);
-        // Train the net what the outputs should have been:
-        data.getTargetOutputs(targetVals);
-        data.writeEvolution("Targets:", targetVals);
-        assert(targetVals.size() == topology.back());
-        myNet.backProp(targetVals);
-        // Report how well the training is working, average over recent samples:
-        data.writeOutputError(myNet.getRecentAverageError());
-
-       // bar.update(increment);
-       // bar.print();
+        trainingPass = 0;
+        data.clear();
+        data.seek();
     }
+      
     std::cout << std::endl << "Done" << std::endl;
     system("start gnuplot -p gnuplot.txt");
     return 0;
--- src/dataset.hpp
+++ src/dataset.hpp
@@ -14,6 +14,13 @@ public:
     void writeOutputError(const double);
     void writeOutput(std::string);
     void writeEvolution(std::string, std::vector<double> &);
+    inline void clear(void) { m_trainingDataFile.clear(); }
+    inline void seek(void) { 
+        m_trainingDataFile.seekg(0, std::ios::beg); 
+        std::string unused;
+        std::getline(m_trainingDataFile, unused);
+        
+        }
     // Returns the number of input values read from the file:
     unsigned getNextInputs(std::vector<double> &inputVals);
     unsigned getTargetOutputs(std::vector<double> &targetOutputVals);
